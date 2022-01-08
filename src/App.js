64c23ffa2aff59f2018e074830c1c4e710dde537
rituraj567 <riturajkadamati89@gmail.com>
2022-01-08 16:35:48 +0530

    added files

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React, { Component } from "react";
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value }, () => {
+      console.log(this.state);
+    });
+  };
+
+  render() {
+    const { monsters, searchField } = this.state;
+    console.log(monsters);
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder={"Search monsters"}
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters}></CardList>
+      </div>
+    );
+  }
+}
+
+export default App;
